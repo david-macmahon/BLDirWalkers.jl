081e1fedcbfecf677ebae8d5f19c0c98222665ae
David MacMahon <davidm@astro.berkeley.edu>
2026-05-07 15:33:54 -0700

    Initial blpn_datag_seticore.jl update for v0.4.0
    Untested, but ready for testing.

--- bin/blpn_datag_seticore.jl
+++ bin/blpn_datag_seticore.jl
@@ -2,12 +2,21 @@ using Dates
 start = now()
 
 using BLDirWalkers
+using BLDirWalkers: unixms, qstatus, workerhostpid
+using BLDirWalkers.Seticore: AbstractCapnpInfo, HitInfo, StampInfo
+using CSV
 using Distributed
 using Sockets
-using DuckDB, DataFrames
+using DuckDB
 using DuckDB: DBInterface as DBI
 using StructArrays
-import Base: n_avail
+import Profile
+
+#---
+# Refuse to run if running with fewer than 7 threads (1 per queue * 4 queues +
+# 1 for dwtask + for dbtask + minimum 1 for in-process directory agent).
+nthreads = Threads.nthreads()
+nthreads < 7 && error("refusing to run with fewer than 7 threads (nthreads=$nthreads)")
 
 #---
 # Start workers
@@ -40,44 +49,143 @@ import Base: n_avail
 
 ENV["JULIA_WORKER_TIMEOUT"] = 120.0
 
-function start_workers(workerspec; prjdir=dirname(@__DIR__))
-    addprocs(workerspec;
-        # Hack for when running on blph0 (for now...)
-        dir = pwd(),
-        env = ["JULIA_PROJECT"=>prjdir],
-        exeflags = "-t 1"
+function exeflags(; project=dirname(@__DIR__), threads=4)
+    (;
+        exeflags=`--project="$project" --threads=$threads`
     )
 end
 
+function start_workers(workerspec; kwargs...)
+    addprocs(workerspec; exeflags()..., kwargs...)
+end
+
+# At the time of this writing, blpn24 was inaccessible
+silohosts = [
+    ["blpn$i" for i in 0:15],      # /datag0 "silo"
+    ["blpn$i" for i in 16:31 if i != 24], # /datag1
+    ["blpn$i" for i in 32:47],     # /datag2 "silo"
+    ["blpn$i" for i in 48:63],     # /datag3 "silo"
+    ["blpn$i" for i in 64:2:78],   # /datag4 "silo"
+    ["blpn$i" for i in 80:2:94],   # /datag5 "silo"
+    ["blpn$i" for i in 96:2:110],  # /datag6 "silo"
+    ["blpn$i" for i in 112:2:126], # /datag7 "silo"
+]
+
+silospecs = [[(h, :auto) for h in silo] for silo in silohosts]
+#=
 silospecs = [
     [("blpn$i", :auto) for i in 0:15],      # /datag0 "silo"
-    [("blpn$i", :auto) for i in 16:31],     # /datag1 "silo"
+    [("blpn$i", :auto) for i in 16:31 if i != 24], # /datag1
     [("blpn$i", :auto) for i in 32:47],     # /datag2 "silo"
     [("blpn$i", :auto) for i in 48:63],     # /datag3 "silo"
     [("blpn$i", :auto) for i in 64:2:78],   # /datag4 "silo"
-    [("blpn$i", :auto) for i in 80:2:94],   # /datag4 "silo"
-    [("blpn$i", :auto) for i in 96:2:110],  # /datag4 "silo"
-    [("blpn$i", :auto) for i in 112:2:126], # /datag4 "silo"
+    [("blpn$i", :auto) for i in 80:2:94],   # /datag5 "silo"
+    [("blpn$i", :auto) for i in 96:2:110],  # /datag6 "silo"
+    [("blpn$i", :auto) for i in 112:2:126], # /datag7 "silo"
 ]
-#silospecs = [ # For TESTing at Berkeley data center
-#    [("blpc3", 2)],
-#    [("blpc3", 2)]
-#]
+=#
+
+#=
+silospecs = [ # For TESTing at Berkeley data center
+    [("blpc3", 2)],
+    [("blpc3", 2)]
+]
+=#
 
 @info "starting workers"
 oversubcribe = 2
-#@time ws = start_workers(workerspec)
-@time silows = map(silospecs) do hostspecs
-    reduce(vcat, start_workers(hostspecs) for _ in 1:oversubcribe)
+
+@time begin
+    silows = map(silospecs) do hostspecs
+        reduce(vcat, start_workers(hostspecs) for _ in 1:oversubcribe)
+    end
+    # Start queue worker processes on remote hosts
+    # qws is dimensioned [nsilos, 4]
+    # topqs=qws[:,1] run on worker on first host of each silo
+    # dirqs=qws[:,2] run on worker on second host of each silo
+    # fileqs=qws[:,3] run on worker on third host of each silo
+    # outqs=qws[:,4] run on worker on fourth host of each silo
+    qws = mapreduce(hcat, silohosts) do silo
+        start_workers(silo[1:4]; exeflags(threads=:auto)...)
+    end
 end
 
-@info "$(sum(length, silows)) workers started"
+@info "$(sum(length, silows))+$(length(qws)) workers started"
 
 #---
 # Initialize workers
 
 @info "initalize workers"
-@time @everywhere using BLDirWalkers
+@time @everywhere begin
+    using Dates
+    using BLDirWalkers
+    using BLDirWalkers: unixms, qstatus, workerhostpid
+    using BLDirWalkers.Seticore: AbstractCapnpInfo, HitInfo, StampInfo
+    using CSV
+    using DuckDB
+    using DuckDB: DBInterface as DBI
+    using StructArrays
+    import Profile
+
+    # Print profiling data to "$(unixms()).profile"
+    Profile.peek_report[] = ()->Profile.print(joinpath(@__DIR__, "$(unixms()).profile"); groupby=:task)
+
+    function writeheader(io, ::Type{T}) where T<:AbstractCapnpInfo
+        println(io, join(fieldnames(T), ","))
+    end
+
+    # Using a Union ensures that v contains all the same type
+    function writeobjects(io, v::Vector{T}) where T<:Union{HitInfo,StampInfo}
+        CSV.write(io, StructArray(v); append=true)
+    end
+
+    """
+        output_handler(outdir, outq, progress_channel)
+
+    Takes HitInfo/StampInfo objects from outq and writes them to
+    "hits.HOSTNAME.csv" and "stamps.HOSTNAME.csv" in outdir.  Periodically send
+    progress update to `progress_channel`.
+    """
+    function output_handler(outdir, outq, progress_channel)
+        hostname = gethostname()
+        iohits = open(joinpath(outdir, "hits.$hostname.csv"), "w")
+        iostamps = open(joinpath(outdir, "stamps.$hostname.csv"), "w")
+
+        @info "writing to output files"
+        hitcount = 0
+        stampcount = 0
+        try
+            writeheader(iohits, HitInfo)
+            writeheader(iostamps, StampInfo)
+
+            for items in Iterators.takewhile(!isnothing, outq)
+                io = if eltype(items) == HitInfo
+                    # Increment hitcount
+                    hitcount += length(items)
+                    iohits
+                else
+                    # Increment stampcount
+                    stampcount += length(items)
+                    iostamps
+                end
+
+                writeobjects(io, items)
+
+                if (hitcount + stampcount) % 100 == 0
+                    put!(progress_channel, (; time=unixms(), hostname, hitcount, stampcount))
+                end
+            end
+        finally
+            close(iohits)
+            close(iostamps)
+        end
+
+        put!(progress_channel, (; time=unixms(), hostname, hitcount, stampcount, final=true))
+        @info "$hostname done writing to output files ($hitcount, $stampcount)"
+
+        hitcount, stampcount
+    end
+end
 @info "workers initialized"
 
 #---
@@ -86,41 +194,60 @@ end
 topdirs = ["/datag/blpn$i" for i in Iterators.flatten((0:63, 64:2:126))]
 #topdirs = ["/datax/scratch/jwst-test"] # For TESTing at Berkeley data center
 
-#---
-# Create DirWalker queues.
-# dirq is non-remote, but fileq and outq are remote.
-
-# Make RemoteDirQueues and RemoteFilesQueues on first worker of each silo
-dirqs = [RemoteDirQueue(ws[1]; sz=Inf) for ws in silows]
-fileqs = [RemoteFileQueue(ws[1]; sz=Inf) for ws in silows]
-outq = RemoteOutQueue{Vector{<:Seticore.AbstractCapnpInfo}}(; sz=Inf)
+outdir = "/datag/inventory"
 
 #---
-# Start DirWalkers
+# Create DirWalker queues and progress_channel.
+
+# Make RemoteQueues on queue workers of each silo
+topqs = map(qw->RemoteTopQueue(qw; sz=Inf), qws[:,1])
+dirqs = map(qw->RemoteDirQueue(qw; sz=Inf), qws[:,2])
+fileqs = map(qw->RemoteFileQueue(qw; sz=Inf), qws[:,3])
+outqs = map(qw->RemoteOutQueue{Vector{<:Seticore.AbstractCapnpInfo}}(qw; sz=Inf), qws[:,4])
+
+# Output handlers will post progress updates here
+progress_channel = RemoteChannel(()->Channel{NamedTuple}(Inf))
+
+#---
+# Start DirWalkers running on topq workers
+
+# Log worker hosts/PIDs
+open(joinpath(@__DIR__, "workers.txt"), "w") do io
+    println(io, "topqs on ", join(workerhostpid.(qws[:,1])), " ")
+    println(io, "dirqs on ", join(workerhostpid.(qws[:,2])), " ")
+    println(io, "fileqs on ", join(workerhostpid.(qws[:,3])), " ")
+    println(io, "outqs on ", join(workerhostpid.(qws[:,4])), " ")
+end
 
 # filefunc and filepred must be defined @everywhere!
-runtasks = map(zip(silows, dirqs, fileqs)) do (ws, dirq, fileq)
+dwfutures = map(zip(silows, topqs, dirqs, fileqs, outqs)) do (ws, topq, dirq, fileq, outq)
     @spawnat ws[1] run_dirwalker(
-        Seticore.filefunc, dirq, fileq, outq, topdirs;
+        Seticore.filefunc, topq, dirq, fileq, outq, topdirs;
         filepred=Seticore.filepred,
         dagentspec=ws[1:1],
         fagentspec=ws[2:end],
-        extraspec=[]
+        extraspec=ws[1:1]
     )
+end
+
+#---
+# Run output_handler on oqworkers
+
+# Run output_handler on out queue worker
+outputfutures = map(qws[:,4], outqs) do oqworker, outq
+    @spawnat oqworker output_handler(outdir, outq, progress_channel)
 end
 
 #---
 # Connect to database and create tables
 
+#=
 @info "create new database"
 # connect to database (for main task)
-dbfile = "/datag/users/seticoredb/seticorefiles.duckdb"
-#dbfile = "/datax/scratch/davidm/filedb/seticorefiles.duckdb" # TESTing
+#dbfile = "/datag/users/seticoredb/seticorefiles.duckdb"
+dbfile = "/datax/scratch/davidm/filedb/seticorefiles.duckdb" # TESTing
 rm(dbfile; force=true)
 db = DBI.connect(DuckDB.DB, dbfile)
-
-hittab = "seticorehits"
-stamptab = "seticorestamps"
 
 @info "creating table $hittab for HitInfo records"
 @info "creating table $stamptab for StampInfo records"
@@ -133,10 +260,12 @@ DBI.execute(db, "create table $stamptab as select 1 as 'id', * from mockstamp")
 
 DuckDB.unregister_table(db, "mockhit")
 DuckDB.unregister_table(db, "mockstamp")
+=#
 
 #---
 # Define appender functions
 
+#=
 function appendrows(appender, id, itemvec)
     for item in itemvec
         id += 1
@@ -189,20 +318,41 @@ function run_appender(db, outq, npending=1; hittab="seticorehits", stamptab="set
 
     hitid, stampid
 end
+=#
 
 #---
 # Run dirwalker database appender
 
-npending = length(runtasks)
+#=
+npending = length(dwfutures)
 hitcount, stampcount = run_appender(db, outq, npending)
+=#
 
 #---
-# Get stats for the tasks
+# Start progress reporter task
 
+progresstask = Threads.@spawn begin
+    for nt in Iterators.takewhile(!isempty, progress_channel)
+        println(nt)
+    end
+end
+
+#---
+# Wait for completions
+
+#Get stats for the tasks
+silostats = fetch.(dwfutures)
 #stats = map(futures->fetch.(futures), fetch.(fetch(runtask)))
-silostats = map(stats->DataFrame.(stats), fetch.(runtasks))
-dagentstats = mapreduce(first, vcat, silostats)
-fagentstats = mapreduce(last, vcat, silostats)
+#silostats = map(stats->DataFrame.(stats), fetch.(dwfutures))
+#dagentstats = mapreduce(first, vcat, silostats)
+#fagentstats = mapreduce(last, vcat, silostats)
+
+# Get results freom outputfutures
+hitstampcounts = fetch.(outputfutures)
+
+# Inform progress task that the show is over
+put!(progress_channel, (;))
+wait(progresstask)
 
 #---
 # Get stop time and compute elapsed
@@ -210,8 +360,10 @@ fagentstats = mapreduce(last, vcat, silostats)
 stop = now()
 elapsed = canonicalize(stop - start)
 @info "total elapsed time: $elapsed"
-@info "created $(hitcount) hit rows, $(stampcount) stamp rows"
+@info "output handler stats" hitstampcounts
+#=
 @info "dir agent stats (per silo)"
 println(dagentstats)
 @info "file agent stats (per silo)"
 println(fagentstats)
+=#
